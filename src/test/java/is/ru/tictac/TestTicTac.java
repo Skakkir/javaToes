278c3e981efb8370fe3bcdb7cf6e1eb521fbb00c
eythoro16 <eythoro16@ru.is>
2017-10-27 14:42:58 +0000

    new TicTacToe project

---
+++ src/test/java/is/ru/tictac/TestTicTac.java
@@ -0,0 +1,11 @@
+package is.ru.tictac;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+
+public class TestTicTac{
+
+}
