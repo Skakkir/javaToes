0a91e5e3a5e03f93325357294b1238be37538084
bennisverrir <bennisverrir@gmail.com>
2017-10-28 22:49:35 +0000

    added constructors to Board class

--- src/main/java/is/ru/tictac/Board.java
+++ src/main/java/is/ru/tictac/Board.java
@@ -1,7 +1,23 @@
 package is.ru.tictac;
 public class Board {
 
+	private int size = 3;
+	private char[][] board;
+
 	Board(){
 
+		board = new char[size][size];
+		char temp = '1';
+		for (int i = 0; i < size; i++){
+			for (int j = 0; j < size; j++) {
+				board[i][j] = temp;
+				temp++;
+			}
+		}
+	}
+
+	Board(int size, char[][] board){
+		this.size = size;
+		this.board = board;
 	}
 }
