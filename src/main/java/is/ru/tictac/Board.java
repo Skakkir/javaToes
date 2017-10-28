6a5cdb71d495cf00acdd9e56df35a968acade1e9
bennisverrir <bennisverrir@gmail.com>
2017-10-28 23:08:55 +0000

    added VerticalTrue test

--- src/main/java/is/ru/tictac/Board.java
+++ src/main/java/is/ru/tictac/Board.java
@@ -33,5 +33,11 @@ public class Board {
 		}
 		return true;
 	}
+
+	public boolean checkVertical(){
+		return true;
+	}
+
+
 }
 
--- src/test/java/is/ru/tictac/TestBoard.java
+++ src/test/java/is/ru/tictac/TestBoard.java
@@ -12,12 +12,18 @@ public class TestBoard{
 		char[] row = {'x', 'x', 'x'};
 		assertEquals(true, b.checkRow(row));
 	}
-	
+
 	@Test
 	public void TestCheckRowFalse(){
 		Board b = new Board();
 		b.setSize(3);
 		char[] row = {'x', 'o', 'x'};
 		assertEquals(false, b.checkRow(row));	
+	}
+
+	@Test
+	public void TestVerticalTrue(){
+		Board b = new Board(3, new char[][] {{'X', '2', '3'}, {'X', '5', '6'}, {'X', '8', '9'}});
+		assertEquals(true, b.checkVertical());
 	}
 }
