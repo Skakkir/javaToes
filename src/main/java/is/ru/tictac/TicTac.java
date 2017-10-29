349de051c01c6e0639257a24811149183a35796d
kolbeinsson88 <kolbeinsson88@gmail.com>
2017-10-29 06:10:24 +0000

    added the addToboard() w/test

--- src/main/java/is/ru/tictac/TicTac.java
+++ src/main/java/is/ru/tictac/TicTac.java
@@ -1,10 +1,24 @@
 package is.ru.tictac;
+import java.io.IOException;
+import java.util.Scanner;
 
-import static spark.Spark.*;
-import static spark.Spark.get;
+
 public class TicTac {
+
+	private Board board = new Board();
+	private Player currentPlayer = new Player('X');
+	
+	public void addToBoard(int num){
+		board.add(num, currentPlayer);
+	}
+
 	public static void main(String[] args) {
- 
+		TicTac game = new TicTac();
+		game.addToBoard(2);
+		game.addToBoard(3);
+		game.addToBoard(6);
+		
+		game.board.print();
  	}
 
 }
--- src/test/java/is/ru/tictac/TestTicTac.java
+++ src/test/java/is/ru/tictac/TestTicTac.java
@@ -1,11 +1,16 @@
 package is.ru.tictac;
 
+import java.util.Scanner;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
 
 public class TestTicTac{
 
+	@Test
+	public void TestAddToBoard(){
+	  	TicTac game = new TicTac();
+      game.addToBoard(2);
+      Board board = new Board(3, new char[][] {{'1', 'X', '3'}, {'4', '5', '6'}, {'7', '8', '9'}});
+      assertEquals(true, board.checkCellOccupied(0, 1));
+   }
 }
