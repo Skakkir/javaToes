799eea5be51ecb2f7ce40920dd1867738b40f287
kolbeinsson88 <kolbeinsson88@gmail.com>
2017-10-29 00:39:37 +0000

    Configureations for javaToes routing class

--- src/main/java/is/ru/tictac/TicTac.java
+++ src/main/java/is/ru/tictac/TicTac.java
@@ -4,15 +4,7 @@ import static spark.Spark.*;
 import static spark.Spark.get;
 public class TicTac {
 	public static void main(String[] args) {
-    	port(getPort());        
-    	get("/hello", (req, res) -> "Hello Worlds!");
-	}
-	static int getPort() {
-    	ProcessBuilder newProcess = new ProcessBuilder();
-	    if (newProcess.environment().get("PORT") != null) {
-	    	return Integer.parseInt(newProcess.environment().get("PORT"));
-    	}
-     	return 4567;
+ 
  	}
 
 }
---
+++ src/main/java/is/ru/tictac/javaToes.java
@@ -0,0 +1,33 @@
+package is.ru.tictac;
+
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
+
+public class javaToes implements SparkApplication {
+
+
+        public static void main(String[] args) {
+        staticFileLocation("/public");
+        SparkApplication javaToes = new javaToes();
+        port(getPort());                
+        javaToes.init();
+    }
+
+    static int getPort() {
+    	ProcessBuilder newProcess = new ProcessBuilder();
+	    if (newProcess.environment().get("PORT") != null) {
+	    	return Integer.parseInt(newProcess.environment().get("PORT"));
+    	}
+     	return 4567;
+     }
+
+    public void init(){
+        post(
+          "/", 
+          (request, response) -> {
+            return "Hello World";
+        });
+
+    }        
+}
