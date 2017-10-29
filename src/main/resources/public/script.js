78825b75af074f3d490bc8cd3c6cc9b7568a3b37
kolbeinsson88 <kolbeinsson88@gmail.com>
2017-10-29 14:27:08 +0000

    Toggle player status.

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -6,17 +6,35 @@ $(document).ready(function() {
       alert('test');
       var id = this.id.slice(-1);
   
+
+
+      function togglePlayerStatus(data){
+        switch (data) {
+          case 'X':
+            $('#current-player').html('O');      
+            break;
+          case 'O':
+            $('#current-player').html('X');        
+            break;
+          default:
+            $('#status').html('Unknown error!').attr('class',
+            'alert alert-warning');
+            break;
+        }
+      }
+
       $.ajax({
         url: "/",
         dataType: 'text',
         type: "post", 
         data: "id=" + id,
         success: function(data) {        
-          
-         document.getElementById("cID"+id).innerHTML = data;
+          togglePlayerStatus(data);
+          document.getElementById("cID"+id).innerHTML = data;
         },
          error: function(data) {
-  
+          $('#status').html('Error! Request failed.').attr('class',
+          'alert alert-warning');
         }
       });
     });  
